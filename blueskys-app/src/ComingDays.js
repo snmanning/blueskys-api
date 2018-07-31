d6cea57acf88985926bf0359f6c341c618819d5f
Sarah Manning <sarahnmanning@gmail.com>
2018-07-30 21:00:10 -0400

    started future forecast component

--- blueskys-app/src/ComingDays.js
+++ blueskys-app/src/ComingDays.js
@@ -2,3 +2,66 @@ import React, { Component } from 'react';
 import WeatherIcons from 'react-weathericons';
 import './ComingDays.css';
 import axios from 'axios';
+
+class ComingDays extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            daily: {},
+            isLoading: true
+        };
+    }
+
+componentDidMount() {
+    // static location for Starke, FL
+    const lat = '29.9441';
+    const lon = '-82.1098';
+    const url = `/forecast/location/${lat},${lon}`;
+    axios.get(url).then(response => {
+        this.setState({
+            daily: response.data.daily.data,
+            isLoading: false,
+        });
+    }).catch((error) => {
+        this.setState({
+            isLoading: true,
+            error: error,
+        });
+    });
+}
+
+render() {
+    const {isLoading, error, daily} = this.state;
+        if(error) {
+            return (
+                <p>
+                    The future forecast is currently unavailable.
+                    Please try again later.
+                </p>
+            );
+        }
+        if(isLoading) {
+            return (
+                <h2>
+                    Gazing into the crystal ball...
+                </h2>
+            );
+        }
+
+        const lo = Math.round(daily.data.temperatureLow);
+        const hi = Math.round(daily.datta.temperatureHigh);
+
+    return(
+        <div className='ComingDays-container'>
+            <section className='ComingDays-weather' >
+                <WeatherIcons name='darksky' iconId='{daily.data.icon}' />
+                Lo: {lo}F <br/>
+                Hi: {hi}F
+            </section>
+        </div>
+    );
+}
+
+}
+
+export default ComingDays;
